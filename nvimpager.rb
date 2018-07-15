966c64f61f0e34961cec06aa824b7e50a3d332e7
Neville Dalal <github@nevd.org>
2018-07-15 11:00:50 +0100

    functional but with my fork until the next release

--- nvimpager.rb
+++ nvimpager.rb
@@ -2,7 +2,7 @@ class Nvimpager < Formula
   desc "Use nvim as a pager"
   homepage "https://github.com/lucc/nvimpager"
   url "https://github.com/nevd/nvimpager/archive/0.5.1.tar.gz"
-  sha256 "0401d7b382b522035321f6dc653cfa4dfa3cc8f03af03433f6f1b9c759ff082f"
+  sha256 "0f2fafc065e480112f34836a1d5e77312d399713330603b611f2ce806faf1b67"
   head "https://github.com/nevd/nvimpager.git"
   depends_on "pandoc" => :build
   depends_on "neovim"
