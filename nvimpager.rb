ddd606b79cccaeaaea1f282b313dcdbccd3c2443
Neville Dalal <git@nevd.org>
2019-04-05 22:48:33 +0100

    updated for v6.0

--- nvimpager.rb
+++ nvimpager.rb
@@ -1,12 +1,12 @@
 class Nvimpager < Formula
   desc "Use nvim as a pager"
   homepage "https://github.com/lucc/nvimpager"
-  url "https://github.com/lucc/nvimpager/archive/v0.5.1.tar.gz"
-  sha256 "3dbe704f5368fe132853bee8c042d425310bd5676abd1ffb362fa9c55cc3f8ae"
+  url "https://github.com/lucc/nvimpager/archive/v0.6.tar.gz"
+  sha256 "0415a57ae53d20a9ecdc5d85c22a2847319631f31d9c5b09fe22506183148c46"
   head "https://github.com/nevd/nvimpager.git"
   depends_on "pandoc" => :build
   depends_on "neovim"
-  depends_on "gnu-sed"
+  depends_on "lua"
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
